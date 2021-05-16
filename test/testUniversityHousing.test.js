c4f1fa7bf4071de71d86e18a244761ed6107cdd1
jpreciadom <jdiegopm12@gmail.com>
2021-05-16 15:12:01 -0500

    Separación de los contratos y el frontEnd

---
+++ test/testUniversityHousing.test.js
@@ -0,0 +1,131 @@
+const { assert } = require('chai');
+
+const UniversityHousing = artifacts.require('./UniversityHousing.sol');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('UniversityHousing', ([owner1, owner2, renter1, renter2]) => {
+
+    let universityHousing;
+
+    before(async () => {
+        universityHousing = await UniversityHousing.deployed()
+    })
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            const address = await universityHousing.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+    })
+
+    describe('Rent', async () => {
+        let rentCount, rent
+
+        it('Offer a rent', async () => {
+            await universityHousing.postRent(10, { from: owner1 })
+            rentCount = await universityHousing.rentCount()
+            rent = await universityHousing.getRent(rentCount)
+
+            // SUCCESS
+            assert.equal(rent.id, rentCount)
+            assert.equal(rent.owner, owner1)
+            assert.equal(rent.renter, 0x0)
+            assert.equal(rent.rentValue, 10)
+        })
+
+        it('Get a rent by id', async () => {
+            await universityHousing.postRent(10, { from: owner1 })
+            rentCount = await universityHousing.rentCount()
+            
+            // The rent in on the map
+            rent = await universityHousing.getRent(rentCount)
+            assert.notEqual(rent.id, 0)
+            assert.notEqual(rent.owner, 0x0)
+            assert.notEqual(rent.rentValue, 0)
+
+            // The rent is not on the map
+            rent = await universityHousing.getRent(rentCount + 1).should.be.rejected
+        })
+
+        it('Take a rent', async () => {
+            await universityHousing.postRent(10, { from: owner1 })
+            rentCount = await universityHousing.rentCount()
+
+            // SUCCESS
+            await universityHousing.takeRent(rentCount, { from: renter1 })
+            rent = await universityHousing.getRent(rentCount)
+            assert.equal(rent.renter, renter1)
+
+            // FAILURE - The rent has already been taken
+            await universityHousing.takeRent(rentCount, { from: renter2 }).should.be.rejected
+            rent = await universityHousing.getRent(rentCount)
+            assert.notEqual(rent.renter, renter2)
+            assert.equal(rent.renter, renter1)
+
+            // FAILURE - The rent does not exist
+            await universityHousing.takeRent(rentCount + 1, { from: renter1 }).should.be.rejected
+        })
+    })
+
+    describe('Owner options', async () => {
+        let rentCount, rent
+
+        it('Change the rent value', async () => {
+            let firstRentValue = 1000
+            let secondRentValue = 2000
+            let thirdRentValue = 3000
+
+            await universityHousing.postRent(firstRentValue, { from: owner1 })
+            rentCount = await universityHousing.rentCount()
+
+            // SUCCESS
+            await universityHousing.changeRentValue(rentCount, secondRentValue, { from: owner1 })
+            rent = await universityHousing.getRent(rentCount)
+            assert.notEqual(rent.rentValue, firstRentValue)
+            assert.equal(rent.rentValue, secondRentValue)
+
+            // FAILURE - sender is not the owner
+            await universityHousing.changeRentValue(rentCount, thirdRentValue, { from: owner2 }).should.be.rejected
+            rent = await universityHousing.getRent(rentCount)
+            assert.equal(rent.rentValue, secondRentValue)
+            assert.notEqual(rent.rentValue, thirdRentValue)
+
+            // FAILURE - the new value is equal to the previous one
+            await universityHousing.changeRentValue(rentCount, secondRentValue, { from: owner1 }).should.be.rejected
+
+            // FAILURE - The rent does not exist
+            await universityHousing.changeRentValue(rentCount + 1, secondRentValue, { from: owner1 }).should.be.rejected
+        })
+    })
+
+    describe('Renter options', async () => {
+        let rentCount, rent
+
+        it('Leave rent', async () => {
+            await universityHousing.postRent(10, { from: owner1 })
+            rentCount = await universityHousing.rentCount()
+
+            // SUCCESS
+            await universityHousing.takeRent(rentCount, { from: renter1 })
+            await universityHousing.leaveRent(rentCount, { from: renter1 })
+            rent = await universityHousing.getRent(rentCount)
+            assert.equal(rent.renter, 0x0)
+
+            // FAILURE - Sender is not the renter
+            await universityHousing.takeRent(rentCount, { from: renter1 })
+            await universityHousing.leaveRent(rentCount, { from: renter2 }).should.be.rejected
+            rent = await universityHousing.getRent(rentCount)
+            assert.notEqual(rent.renter, 0x0)
+            assert.equal(rent.renter, renter1)
+
+            // FAILURE - The rent does not exist
+            await universityHousing.takeRent(rentCount + 1, { from: renter1 }).should.be.rejected
+        })
+    })
+});
